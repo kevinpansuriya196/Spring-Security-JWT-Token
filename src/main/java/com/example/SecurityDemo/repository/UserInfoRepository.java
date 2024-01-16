158652d5addf1abda2aa5cd8b8f8214f61b0856f
kevinpansuriya196 <kevin.pansuriya196@gmail.com>
2024-01-16 14:38:15 +0530

    update

--- src/main/java/com/example/SecurityDemo/repository/UserInfoRepository.java
+++ src/main/java/com/example/SecurityDemo/repository/UserInfoRepository.java
@@ -1,7 +1,6 @@
 package com.example.SecurityDemo.repository;
 
 import com.example.SecurityDemo.entity.UserInfo;
-//import com.ey.springboot3security.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,10 +12,11 @@ import java.util.Optional;
 public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
     Optional<UserInfo> findByName(String username);
 
-    //    SELECT * FROM security.users where roles="ADMIN";
-    @Query( nativeQuery = true, value = "SELECT * FROM security.users where roles='USER' ")
+    //    SELECT * FROM security.users where roles="USER";
+    @Query(nativeQuery = true, value = "SELECT * FROM security.users where roles='USER' ")
     List<UserInfo> FindUsers();
 
-    @Query( nativeQuery = true, value = "SELECT * FROM security.users where roles='ADMIN' ")
+    //    SELECT * FROM security.users where roles="ADMIN";
+    @Query(nativeQuery = true, value = "SELECT * FROM security.users where roles='ADMIN' ")
     List<UserInfo> FindAdmins();
 }
--- src/main/java/com/example/SecurityDemo/service/JwtService.java
+++ src/main/java/com/example/SecurityDemo/service/JwtService.java
@@ -18,6 +18,7 @@ import java.util.function.Function;
 public class JwtService {
 
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
@@ -33,7 +34,7 @@ public class JwtService {
     }
 
     private Key getSignKey() {
-        byte[] keyBytes= Decoders.BASE64.decode(SECRET);
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
